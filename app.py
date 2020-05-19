c3a15089839dc04cc698f7e2507e58e4d57d3ab6
snellings18 <65515625+snellings18@users.noreply.github.com>
2020-05-19 16:02:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,21 +19,21 @@ def profile(name):
 	return render_template('index.html', name=new_name)
 	
 	
-@app.route('/multiply_numbers', methods=['GET','POST'])
-def multiply_numbers_post():
+@app.route('/add_numbers', methods=['GET','POST'])
+def add_numbers_post():
 	  # --> ['5', '6', '8']
 	  # print(type(request.form['text']))
 	  if request.method == 'GET':
-	  	return render_template('multiply_numbers.html')
+	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
   	      total = 1
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		total *= int(str_num)
-  	      	return render_template('multiply_numbers.html', result=str(total))
+  	      	return render_template('add_numbers.html', result=str(total))
   	      except ValueError:
-  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please."
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 	
 
 @app.route('/shopping_list', methods=['GET','POST'])
